70b06322a46b48b170f751b33ff5ed5273ae883b
Nishant Sharma <you@example.com>
2021-07-30 18:55:36 +0530

    add scripting start

--- app.js
+++ app.js
@@ -176,6 +176,8 @@ app.use((err, req, res, next)=>{
     res.status(statusCode).render('error',{err});
 });
 
-app.listen(3000, ()=>{ 
-   console.log("Server Started....");
+const port = process.env.PORT || 3000 
+
+app.listen(port, ()=>{ 
+   console.log(`Server Started....Serving on port ${port}`);
 });
