a2ae1fb130a3d09c471685874e514b7a18ad5c6d
nishant1931 <nishoo1902>
2021-12-08 14:40:43 +0530

    db.On

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
@@ -10,7 +10,7 @@ const path = require('path');
 const Joi = require('joi');
 const session = require('express-session');
 const flash = require('connect-flash');
-const {campgroundSchema, reviewSchema} = require('./schema.js');
+const { campgroundSchema, reviewSchema } = require('./schema.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const mongoose = require('mongoose');
@@ -29,14 +29,14 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
- 
+
 
 const MongoDBStore = require('connect-mongo')(session);
 
 // const dbUrl = process.env.db_url;
 
 
- const dbUrl = process.env.db_url || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.db_url || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -46,28 +46,28 @@ mongoose.connect(dbUrl, {
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", ()=>{
+db.on("open", () => {
     console.log("Database Connected!!");
 });
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
-const secret= process.env.SECRET || 'thisisasecret';
- 
+const secret = process.env.SECRET || 'thisisasecret';
+
 
 const store = new MongoDBStore({
     url: dbUrl,
     secret,
-    touchAfter: 24* 60* 60
+    touchAfter: 24 * 60 * 60
 });
 
-store.on("error", function(e){
+store.on("error", function (e) {
     console.log('SESSION STORE ERROR', e);
 });
 
@@ -81,8 +81,8 @@ const sessionConfig = {
     cookie: {
         httpOnly: true,
         // secure: true,
-        expires: Date.now() + 1000*60*60*24*7,
-        maxAge: 1000*60*60*24*7
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig));
@@ -96,7 +96,7 @@ const scriptSrcUrls = [
     "https://kit.fontawesome.com/",
     "https://cdnjs.cloudflare.com/",
     "https://cdn.jsdelivr.net",
-     
+
 ];
 const styleSrcUrls = [
     "https://kit-free.fontawesome.com/",
@@ -142,15 +142,15 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-     res.locals.currentUser = req.user;
-     res.locals.success = req.flash('success');
-     res.locals.error = req.flash('error');
-     next();
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 });
 
-app.get('/fakeuser', async(req,res)=>{
-    const user = new User({email: 'colt@gmail.com', username: 'Colt'});
+app.get('/fakeuser', async (req, res) => {
+    const user = new User({ email: 'colt@gmail.com', username: 'Colt' });
     const newUser = await User.register(user, 'chicken');
     res.send(newUser);
 })
@@ -160,24 +160,24 @@ app.use('/campgrounds/:id/reviews', reviewRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 app.use('/', userRoutes);
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render('home');
 })
 
 
 
-app.all('*', (req,res,next)=>{
+app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found!', 404));
 })
 
-app.use((err, req, res, next)=>{
-    const {statusCode = 500 } = err;
-    if(!err.message) err.message = 'OHH no!!!Something went wrong' ;
-    res.status(statusCode).render('error',{err});
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'OHH no!!!Something went wrong';
+    res.status(statusCode).render('error', { err });
 });
 
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
 
-app.listen(port, ()=>{ 
-   console.log(`Server Started....Serving on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server Started....Serving on port ${port}`);
 });
